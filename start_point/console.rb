5ec8ee82984907235179babe60e5fb050cee5410
Jaime Lopez <detec.azul@gmail.com>
2018-03-23 12:43:36 +0000

    Fixed bug on console.rb not creating tickets due to typo

--- start_point/console.rb
+++ start_point/console.rb
@@ -3,6 +3,7 @@ require_relative('./models/customer')
 require_relative('./models/film')
 require_relative('./models/ticket')
 
+Ticket.delete_all()
 Film.delete_all()
 Customer.delete_all()
 
@@ -16,6 +17,14 @@ customer01.save()
 customer02.save()
 film01.save()
 film02.save()
+
+ticket01 = Ticket.new({ 'film_id' => film01.id, 'customer_id' => customer01.id })
+ticket02 = Ticket.new({ 'film_id' => film01.id, 'customer_id' => customer01.id })
+ticket03 = Ticket.new({ 'film_id' => film02.id, 'customer_id' => customer02.id })
+
+ticket01.save()
+ticket02.save()
+ticket03.save()
 
 binding.pry
 nil
