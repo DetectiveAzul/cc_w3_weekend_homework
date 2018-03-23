3be9a0b9859c185573bea0946ae7d2642f1a40b2
Jaime Lopez <detec.azul@gmail.com>
2018-03-23 12:35:22 +0000

    CRUD for the tickets

--- start_point/models/ticket.rb
+++ start_point/models/ticket.rb
@@ -9,4 +9,50 @@ class Ticket
     @customer_id = options['customer_id'].to_i
   end
 
+  def self.all()
+    sql = "SELECT * FROM tickets;"
+    tickets = SqlRunner.run(sql)
+    return tickets.map { |ticket| Ticket.new(ticket) }
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM tickets;"
+    SqlRunner.run(sql)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM tickets
+    WHERE id = $1;"
+    values = [id]
+    ticket_array = SqlRunner.run(sql, values)
+    return Ticket.new(ticket_array.first) unless ticket_array.first == nil
+  end
+
+  def save()
+    sql = "INSERT INTO tickets
+    (film_id, customer_id)
+    VALUES
+    ($1, $2)
+    RETURNING id
+    ;"
+    values = [@film_id, @customer_id]
+    id_array = SqlRunner.run(sql, values)
+    @id = id_array.first['id'].to_i
+  end
+
+  def update()
+    sql = "UPDATE tickets
+    SET (film_id, customer_id) = ($1, $2)
+    WHERE id = $3"
+    values = [@film_id, @customer_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE from tickets
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
 end
