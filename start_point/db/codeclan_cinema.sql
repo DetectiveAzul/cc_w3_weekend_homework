9e85c7de9311a8d01da1c6c063e9bf1ee01108c2
Jaime Lopez <detec.azul@gmail.com>
2018-03-23 10:36:10 +0000

    db creator file done. Sql runner class done

---
+++ start_point/db/codeclan_cinema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS films;
+
+CREATE TABLE films (
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT
+);
+CREATE TABLE customers (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT
+);
+CREATE TABLE tickets (
+  id SERIAL8 PRIMARY KEY,
+  film_id INT8 REFERENCES films(id) ON DELETE CASCADE,
+  customer_id INT8 REFERENCES films(id) ON DELETE CASCADE
+);
---
+++ start_point/db/sql_runner.rb
@@ -0,0 +1,16 @@
+require('pg')
+
+class SqlRunner
+  
+  def self.run(sql, values = [])
+    begin
+      db = PG.connect({ dbname: 'codeclan_cinema', localhost: 'localhost' })
+      db.prepare("query", sql)
+      result = db.exec_prepared("query", values)
+    ensure
+      db.close() unless db == nil
+    end
+    return result
+  end
+
+end
